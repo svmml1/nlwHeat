d81d66f6ee9bf24583ca22f32c09b68a1b5fe21b
Sergio Victor <sergiovmml@gmail.com>
2021-10-31 14:24:15 -0300

    1

--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,20 +1,41 @@
-import "dotenv/config"
-import express, { request, response } from "express";
-import { router } from './routes'
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+
+import { router } from "./routes";
 
 const app = express();
-app.use(express.json())
+app.use(cors());
 
-app.use(router)
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
-    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
 
-app.get("/signin/callback", (request, response) =>{
-    const { code } = request.query;
+  return response.json(code);
+});
 
-    return response.json(code); 
-})
-app.listen(4000, () => console.log('Servidor rodando na porta 4000'))
+export { serverHttp, io };
